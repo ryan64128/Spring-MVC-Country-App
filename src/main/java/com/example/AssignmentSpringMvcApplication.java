670cd1f37afd0286bca0e535fca8cf7e79ca6a83
ryan64128 <ryan64128@yahoo.com>
2021-07-15 00:20:05 -0400

    added comment to app main

--- src/main/java/com/example/AssignmentSpringMvcApplication.java
+++ src/main/java/com/example/AssignmentSpringMvcApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AssignmentSpringMvcApplication {
-
+	// invoke spring application
 	public static void main(String[] args) {
 		SpringApplication.run(AssignmentSpringMvcApplication.class, args);
 	}
